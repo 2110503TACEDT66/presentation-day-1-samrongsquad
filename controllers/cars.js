d3280da743b02da1a3597ff817274f67abd5fa3e
thipbadee <146174087+thipbadee@users.noreply.github.com>
2024-02-26 02:13:22 +0700

    ch6 fix

--- controllers/cars.js
+++ controllers/cars.js
@@ -2,31 +2,31 @@
 //@route    Get /api/v1/cars
 //@access   Public
 exports.getCars = (req,res,next) => {
-    res.status(200).json({success:true, msg:'Show all cars'});
+    res.status(200).json({success:true, msg:'Get all cars'});
 };
 
 //@desc     Get single car
 //@route    Get /api/v1/cars/:id
 //@access   Public
 exports.getCar = (req,res,next) => {
-    res.status(200).json({success:true, msg:`Show car ${req.params.id}`});
+    res.status(200).json({success:true, msg:`Get car ${req.params.id}`}); 
 };
 
-//@desc     Create new car
+//@desc     Create a car
 //@route    POST /api/v1/cars
 //@access   Private
 exports.createCar = (req,res,next) => {
-    res.status(200).json({success:true, msg:'Create new cars'});
+    res.status(200).json({success:true, msg:'Create a car'});
 };
 
-//@desc     Update car
+//@desc     Update single car
 //@route    PUT /api/v1/cars/:id
 //@access   Private
 exports.updateCar = (req,res,next) => {
     res.status(200).json({success:true, msg:`Update car ${req.params.id}`});
 };
 
-//@desc     Delete car
+//@desc     Delete single car
 //@route    DELETE /api/v1/cars/:id
 //@access   Private
 exports.deleteCar = (req,res,next) => {

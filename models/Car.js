39fa834d3ab603e0281d8f0ae4fa93db9152fe03
thipbadee <146174087+thipbadee@users.noreply.github.com>
2024-02-27 01:55:04 +0700

    finish ch7

--- models/Car.js
+++ models/Car.js
@@ -1,17 +1,51 @@
 const mongoose = require('mongoose');
 
+//name address district province postalcode tel type price licenseplate
 const CarSchema = new mongoose.Schema({
-    brand :{
+    name: {
         type: String,
-        required: [true, 'Please add a brand'],
+        required: [true, 'Please add a name'],
         unique: true,
         trim: true,
         maxlength:[50, 'Name can not be more than 50 charracters']
     },
+    address:{
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    district:{
+        type: String,
+        required: [true, 'Please add a district']
+    },
+    province:{
+        type: String,
+        required: [true, 'Please add a province']
+    },
+    postalcode:{
+        type: String,
+        required: [true, 'Please add a postalcode'],
+        maxlength: [5,'Postal Code can not be more than 5 digits']
+    },
+    tel:{
+        type: String
+    },
+    brand :{
+        type: String
+       
+    },
     type :{
         type: String,
+        enum: ['sedan','sport','truck'],
         required: [true,'Please add a type']
+    },
+    price :{
+        type: String
+    },
+    licenseplate :{
+        type:String,
+        unique:true
     }
+
 });
 
 module.exports=mongoose.model('Car',CarSchema);

eec6899a36ab828f9d1f0d6842471e79352bfb3d
chulinuwuw <ananzxzoxd@gmail.com>
2024-02-27 18:32:44 +0700

    TOKEN / Autherization

--- routes/cars.js
+++ routes/cars.js
@@ -2,8 +2,8 @@ const express = require('express');
 const {getCars, getCar, createCar, updateCar, deleteCar} = require('../controllers/cars');
 const router = express.Router();
 //router มีหน้าที่ส่งต่อ request ให้กับ method ที่เกี่ยวข้อง (ซึ่งเป็น method ที่อยู่ในส่วนของ cars)
-
+const {protect,authorize} = require('../middleware/auth');
 router.route('/').get(getCars).post(createCar);
-router.route('/:id').get(getCar).put(updateCar).delete(deleteCar);
+router.route('/:id').get(getCar).put(updateCar).delete(protect,authorize(`admin`),deleteCar);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const cookieParser = require('cookie-parser');
+const auth = require('./routes/auth');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
@@ -12,9 +14,11 @@ const app = express();
 
 //Body parser
 app.use(express.json());
+app.use(cookieParser());
 
 const cars = require('./routes/cars');
-app.use('/api/v1/cars',cars)
+app.use('/api/v1/cars',cars);
+app.use('/api/v1/auth',auth);
     
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));

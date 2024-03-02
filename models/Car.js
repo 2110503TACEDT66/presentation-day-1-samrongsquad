c386e79d8ed82899870e367e5a6a9ccfe3e30a17
Amphi <6633287021@student.chula.ac.th>
2024-03-02 23:06:02 +0700

    fix: can add googleMapsURL and imageURL with the right format of URL link

--- models/Car.js
+++ models/Car.js
@@ -2,58 +2,78 @@ const mongoose = require('mongoose');
 
 //name address district province postalcode tel type price licenseplate
 const CarSchema = new mongoose.Schema({
-    nameofmodel: {
-        type: String,
-        required: [true, 'Please add a name of model'],
-        trim: true,
-        maxlength: [50, 'Name can not be more than 50 characters']
-    },
     brand: {
         type: String,
-        required: [true, 'Please add a brand']
+        enum: ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'BMW', 'Mercedes-Benz', 'Audi', 'Nissan',
+        'Volkswagen', 'Tesla', 'Lexus', 'Subaru', 'Mazda', 'Hyundai', 'Kia', 'Volvo',
+        'Porsche', 'Jaguar', 'Land Rover', 'Jeep', 'Ram', 'Chrysler', 'Dodge', 'Cadillac',
+        'Buick', 'GMC', 'Acura', 'Infiniti', 'Mitsubishi', 'Mini', 'Fiat', 'Alfa Romeo',
+        'Genesis', 'Smart', 'Lincoln', 'Bentley', 'Ferrari', 'Maserati', 'McLaren', 'Rolls-Royce',
+        'Bugatti', 'Lamborghini', 'Aston Martin', 'Lotus', 'Koenigsegg', 'Pagani', 'Maybach',
+        'Saab', 'Saturn', 'Pontiac', 'Hummer', 'Oldsmobile', 'Plymouth', 'Datsun', 'Isuzu',
+        'Suzuki', 'Scion', 'Geo', 'AMC', 'Studebaker', 'Hudson', 'DeLorean', 'Packard'],
+        required: [true, 'Please add a name of car brand.']
+    },
+    model: {
+        type: String,
+        required: [true, 'Please add a name of model.'],
+        trim: true,
+        maxlength: [50, 'Name of model can not be more than 50 characters.']
     },
     type: {
         type: String,
-        enum: ['sedan', 'sport', 'truck', 'hatchback', 'convertible', 'suv', 'coupe', 'van', 'minivan', 'pickup', 'electric', 'hybrid', 'luxury', 'crossover'],
-        required: [true, 'Please add a type']
+        enum: ['sedan', 'sport', 'truck', 'hatchback', 'convertible', 'suv', 'coupe', 
+        'van', 'minivan', 'pickup', 'electric', 'hybrid', 'luxury', 'crossover'],
+        required: [true, 'Please add a type.']
     },
-    price: {
+    pricePerDay: {
         type: Number,
         required: true,
-        min: [0, 'Price must be greater than or equal to 0']
+        min: [0, 'Price per day must be greater than or equal to 0.']
     },
-    licenseplate :{
+    licensePlate :{
         type: String,
         unique: true
-        // unique:true,
-        // match: [
-        //     /^[1-9ก-ฮ][ก-ฮ][ -]?[0-9]{1,4}$/,
-        //     'Please add a valid license plate number'
-        // ]
     },
     address: {
         type: String,
-        required: [true, 'Please add an address']
+        required: [true, 'Please add an address.']
     },
     district: {
         type: String,
-        required: [true, 'Please add a district']
+        required: [true, 'Please add a district.']
     },
     province: {
         type: String,
-        required: [true, 'Please add a province']
+        required: [true, 'Please add a province.']
     },
     postalcode: {
         type: String,
-        required: [true, 'Please add a postalcode'],
-        maxlength: [5,'Postal Code can not be more than 5 digits']
+        required: [true, 'Please add a postalcode.'],
+        maxlength: [5,'Postal Code cannot be more than 5 digits.']
+    },
+    googleMapsURL: {
+        type: String,
+        required: [true, 'Please add an Google Maps URL.'],
+        match: [
+            /^https?:\/\/(www\.)?google\.com\/maps\/(?:.*?\&)?(q|daddr)=(.*)$/,
+            'Please add a valid Google Maps URL.'
+        ]
+    },
+    imageURL: {
+        type: String,
+        required: [true, 'Please add an image URL'],
+        match: [
+            /^https?:\/\/.*\.(jpg|jpeg|png|webp|avif|gif|svg)(\?.*)?$/i,
+            'Please add a valid image URL.'
+        ]
     },
     tel: {
         type: String,
-        required: [true, 'Please add a telephone number'],
+        required: [true, 'Please add a telephone number.'],
         match: [
             /^0[0-9]{9}$/,
-            'Please add a valid telephone number'
+            'Please add a valid telephone number.'
         ]
     }
 
